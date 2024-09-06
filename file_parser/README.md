3427e981e72d461f878d9c9a6fd81d639e539a7a
AryaJawarkar <aryajawarkar7@gmail.com>
2024-09-07 00:15:32 +0530

    file-parser

---
+++ file_parser/README.md
@@ -0,0 +1,15 @@
+# FILE PARSER
+
+- This is program which parses a csv file.According to some condition.
+- The condition is gender should be **F** and year should be **2024**
+- The degree should be **Msc**
+
+## PROGRAM
+
+- Program takes a **csv file** as input in **command line arguement**.
+  **python3 file_parser.py input-file-name**
+  **The format of file should be**
+  _RollNo_, _FirstName_ _MiddleName_ _LastName_, _Gender_, _Year_, _Degree_
+
+# To test
+- Run python3 file_parser.py bkp
---
+++ file_parser/file_parser.py
@@ -0,0 +1,47 @@
+import sys
+
+arguements = sys.argv
+
+firstname = '';
+
+
+if len(arguements) != 2:
+    print("less arguements");
+
+with open(arguements[1],'r') as file:
+    for line in file:
+        flag = 1;
+        gender = '';
+        degree = '';
+        year = '';
+        count = 0;
+        line.strip();
+        for char in line:
+            if(count == 4 and char != "\n"):
+                degree += char;
+            if char == ',':
+                count += 1;
+        count = 0;
+        for char in line:
+            if(count == 2 and char != ','):
+                gender += char;
+            if char == ',':
+                count += 1;
+        count = 0;
+        for char in line:
+            if(count == 3 and char != ','):
+                year += char;
+            if char == ',':
+                count += 1;
+        count = 0;
+        #print(degree+"-"+year+"-"+gender+"\n");
+        if degree == 'Msc' and  year == '2024' and gender == 'F':
+            for char in line:
+                if(count == 1 and flag == 1):
+                    firstname += char;
+                if char == ',':
+                    count += 1;
+                if(char == ' '):
+                    flag = 0;
+    print(firstname);
+file.close();
